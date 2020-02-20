f3b64b3ac51b060e8c68134e04bbda58e2de29c6
Diana Badas <diana.badas@gmail.com>
2020-02-20 17:38:54 +0000

    Adding basket inside initialization to start with an empty basket when initialized

--- lib/basket.rb
+++ lib/basket.rb
@@ -5,22 +5,23 @@ require "Large_purchase_promotion"
 
 class Basket
 
-    @@basket = []
+    attr_reader :basket, :promotions
 
     def initialize
+        @basket = []
         @promotions = [Multi_purchase_promotion.new("Very Cheap Chair", 2, 0.75), Large_purchase_promotion.new(60, 10)]
     end
 
     def scan(product)
-        @@basket << product
+        @basket << product
     end
 
     def total
         total = 0
-        @@basket.each { |p| total += p.price }
+        @basket.each { |p| total += p.price }
 
         for promotion in @promotions do
-            total = promotion.apply_promotion(@@basket, total)
+            total = promotion.apply_promotion(@basket, total)
         end
         total.round(2)
     end
