128dcdb755b4a2ed86fafe668a4d2e9b0a972295
Diana Badas <diana.badas@gmail.com>
2020-02-20 17:55:53 +0000

    Rename checkout_spec to basket_spec and add more tests

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -6,8 +6,33 @@ describe Basket do
         @product1 = Product.new('001', 'Very Cheap Chair', 9.25)
         @product2 = Product.new('002', 'Litte table', 45)
         @product3 = Product.new('003', 'Funky Light', 19.95)
+    end
 
-        @@basket = []
+    it "has an empty basket when initialized" do
+        expect(subject.basket).to eq([])
+    end
+
+    it 'can scan an item' do
+        subject.scan(@product1)
+        expect(subject.basket.length).to eq 1
+    end
+
+    it 'does not apply any discount if quantity is not enough' do
+        subject.scan(@product1)
+        subject.scan(@product2)
+
+        total = subject.total
+        
+        expect(total).to eq 54.25
+    end
+
+    it 'does not apply any discount if item is not in offer' do
+        subject.scan(@product3)
+        subject.scan(@product3)
+
+        total = subject.total
+        
+        expect(total).to eq 39.9
     end
 
     it "can apply 10% off when spend over £60" do
